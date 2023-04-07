d575b0e381ce07f3317f58cc430d948fd510b4c2
Keith JOHNSON <keithjohnson.berkeley.edu>
2023-04-07 09:34:03 -0700

    Added messages.js

---
+++ js/messages.js
@@ -0,0 +1,21 @@
+
+var messages_english = {
+    incorrect: "That response was incorrect",
+    slow: "That response was too slow",
+    fast: "That response was too fast",
+    wait: "Please wait",
+    unallowed_zm: " Press either \'z\' or \'m\' ",
+    unallowed_17: " Press a number between 1 and 7 "
+};
+
+var messages_nepali = {
+    incorrect: "त्यो जवाफ गलत थियो।",
+    slow: "तपाईंले धेरै ढिलो जवाफ दिनुभयो।",
+    fast: "तपाईंले धेरै चाँडो जवाफ दिनुभयो",
+    wait: "कृपया पर्खनुहोस्",
+    unallowed_zm: " Press either \'z\' or \'m\' ",
+    unallowed_17: " Press a number between 1 and 7 "
+};
+
+// choose which language will be used
+var messages = messages_nepali;
